5c32bbeb1b68836d1dc04b6e41d4f6c263924f98
g322h <g2203h@gmail.com>
2022-04-02 13:36:28 +0300

    finished banner section

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -5,19 +5,35 @@ import "./Banner.scss";
 
 export const Banner = () => {
     return (
-        <div className="banner">
+        <section className="banner">
             <div className="container">
-                <div className="banner__info">
-                    <h1 className="banner__info-title">Салон красоты Simada</h1>
-                    <p className="banner__info-text">
-                        Cтрижки, окрашивания, укладки, макияж, маникюр и
-                        педикюр, косметология, перманентный макияж, массаж,
-                        коррекция фигуры
-                    </p>
-                    <ButtonRed>получить скидку 20% на первый визит</ButtonRed>
-                    <ButtonWhite>записаться онлайн</ButtonWhite>
+                <div className="banner__inner">
+                    <div className="banner__info">
+                        <h1 className="banner__info-title">
+                            Салон красоты Simada
+                        </h1>
+                        <p className="banner__info-text">
+                            Cтрижки, окрашивания, укладки, макияж, маникюр и
+                            педикюр, косметология, перманентный макияж, массаж,
+                            коррекция фигуры
+                        </p>
+                        <ButtonRed>
+                            получить скидку 20% на первый визит
+                        </ButtonRed>
+                        <ButtonWhite>записаться онлайн</ButtonWhite>
+                    </div>
+                    <ul className="banner__connection">
+                        <li className="banner__connection-item">
+                            ул. Михалковская, 1/51, м. Коптево
+                            <a href="#">+7(495) 947-77-87</a>
+                        </li>
+                        <li className="banner__connection-item">
+                            Пятницкое шоссе, д.9 м. Митино, м. Волоколамская
+                            <a href="#">+7 (495) 925-12-11</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
-        </div>
+        </section>
     );
 };
