2682bfa8ad658d2be23656612064e84201c45f50
g322h <g2203h@gmail.com>
2022-04-03 15:07:09 +0300

    finished header adaptive

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.scss";
 import Logo from "../../shared/images/header/header-logo.png";
 import { HeaderNav } from "./components/HeaderNav";
 import { ButtonRed } from "../Buttons/ButtonRed";
 
 export const Header = () => {
+    const [menu, setMenu] = useState(false);
+
     return (
         <div className="header">
             <div className="container">
@@ -12,12 +14,32 @@ export const Header = () => {
                     <a href="/" className="header__logo">
                         <img src={Logo} alt="Logo" />
                     </a>
-                    <HeaderNav />
-                    <div className="header__schedule">
-                        <span>ПН. - ВС. 10:00 - 22:00</span>
-                        <ButtonRed>записаться</ButtonRed>
+                    <div
+                        className={
+                            menu ? "header__info active" : "header__info"
+                        }
+                    >
+                        <HeaderNav />
+                        <div className="header__schedule">
+                            <span>ПН. - ВС. 10:00 - 22:00</span>
+                            <ButtonRed>записаться</ButtonRed>
+                        </div>
                     </div>
+                    <button
+                        onClick={() => setMenu(!menu)}
+                        className={
+                            menu ? "header__burger active" : "header__burger"
+                        }
+                    >
+                        <span></span>
+                    </button>
                 </div>
+                {menu && (
+                    <div
+                        onClick={() => setMenu(false)}
+                        className="header__overlay"
+                    ></div>
+                )}
             </div>
         </div>
     );
